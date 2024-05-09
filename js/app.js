e7f707040092db18ce17378a3d3d4b7997723d4f
isragonzalez <isra.87@outlook.com>
2024-05-08 23:35:12 -0600

    Landing page terminado

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// VAR
+"use strict";
+const formInput = document.getElementById("form-input");
+const btnSend = document.getElementById("sendForm");
+
+// functions
+formInput.addEventListener("submit",(e)=>{
+    e.preventDefault();
+})
+
+btnSend.addEventListener("click",()=>{
+ console.log("construction in progress");
+})
